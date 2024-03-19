df593b09163ae3d6355ec6b0387428ed24fb85a1
Praise Yu <pyu7@nait.ca>
2024-03-19 12:58:00 -0600

    finished task 1, starting on task 2

--- js/index.js
+++ js/index.js
@@ -8,3 +8,58 @@
   <td>NUMBER OF RATINGS HERE</td>
 </tr> 
 */
+
+let albumStore;
+
+const albumForm = document.getElementById('album-search-form');
+
+//fetch album data
+async function appInit() {
+  const response = await fetch('public/data/albums.json')
+  const data = await response.json()
+  albumStore = [...data]
+  render(data, document.getElementById('album-rows'));
+
+
+
+}
+
+function render(data, container) {
+  console.log(data)
+  data.forEach((item) => {
+    const template = `<tr>
+    <td>${item.album}</td>
+    <td>${item.releaseDate}</td>
+    <td>${item.artistName}</td>
+    <td>${item.genres}</td>
+    <td>${item.averageRating}</td>
+    <td>${item.numberRatings}</td>
+  </tr>`
+    container.insertAdjacentHTML('afterbegin', template);
+  })
+}
+
+
+appInit();
+
+albumForm.addEventListener('submit', function onSubmitSearch(e) {
+  e.preventDefault();
+  // get values from input fields
+  const textInput = e.currentTarget[0].value;
+  const numberInput = e.currentTarget[1].value;
+  if (textInput != null) {
+    getAlbumByNameOrArtist(data, textInput);
+  }
+});
+
+function getAlbumByNameOrArtist(data, textInputValue) {
+  const albums = data;
+  const query = textInputValue;
+
+  const results = albums.filter((album) => {
+    if (albums.album === query || albums.artistName === query) {
+      return album;
+    }
+  })
+
+};
